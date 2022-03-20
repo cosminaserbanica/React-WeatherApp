3c163e7cf909fca5b3e8006e38415a1c999513ab
cosminaserbanica <cosmina.serbanica@yahoo.com>
2022-03-20 18:40:29 +0000

    Add api info

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const api = {
+  key: "69a47be4157c1e0910e0208fa96c51e5",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hiiiiii</h1>
     </div>
   );
 }
